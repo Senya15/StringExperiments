72addeb215dc310223b268de687fe4d489b6ae6a
337-1 <senyuk@bsuir.by>
2022-01-13 16:33:59 +0300

    add changes (Work)

--- src/Loader.java
+++ src/Loader.java
@@ -1,18 +1,14 @@
 
 public class Loader {
     public static void main(String[] args) {
-        String text = "    Вася  заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей     ";
-        char part = ' ';
-
-
+        String text = "    Вася заработал 504500 рублей, Петя - 7563 рубля, а Маша - 30000 рублей     ";
+        char space = ' ';
 
         /*String numberOnly = text.replaceAll("[^0-9]", "");
         System.out.println(numberOnly);*/
 
         printWithIndexes(text);
-        System.out.print("Сумма зароботка всех ребят составляет: ");
-        System.out.println(sumNumbers(text, part));
-
+        System.out.print("Сумма зароботка всех ребят составляет: " + sumNumbers(text, space) + " рублей");
     }
 
     private static int sumNumbers(String text, char part) {
@@ -20,7 +16,7 @@ public class Loader {
         int startIndex = 0;
         int endIndex = 0;
         String serchNumbers;
-        int numbers = 0;
+        int sumNumbers = 0;
         while (!(startIndex == -1)) {
             startIndex = text.indexOf(part, endIndex);
             endIndex = text.indexOf(part, startIndex + 1);
@@ -29,12 +25,12 @@ public class Loader {
             }
             serchNumbers = text.substring(startIndex + 1, endIndex);
             try {
-                numbers += Integer.parseInt(serchNumbers);
+                sumNumbers += Integer.parseInt(serchNumbers);
             } catch (NumberFormatException nfe) {
-                numbers += 0;
+                sumNumbers += 0;
             }
         }
-        return numbers;
+        return sumNumbers;
     }
 
 
